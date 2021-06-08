b418ca2839688ec15984d93903714b165ac96158
Valeria Kan <kan.valeria1309@gmail.com>
2021-06-08 13:58:58 -0600

    Custom hook: useInput

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react'
+
+// Custom hook - watching for param
+function useLogger(value) {
+	useEffect(() => {
+		console.log('Value changed: ', value);
+	}, [value])
+}
+
+function useInput(initialValue) {
+	const [value, setValue] = useState(initialValue)
+
+	const onChange = event => {
+		setValue(event.target.value)
+	}
+
+	const clear = () => setValue('')
+
+	return {
+		bind: {value, onChange},
+		value, 
+		clear
+	}
+}
 
 function App() {
-	const [name, setName] = useState('');
-	const [lastName, setLastName] = useState('');
+	const input = useInput('')
+	const lastName = useInput('')
 
-	const changeHandler = event => {
-		setName(event.target.value)
-	};
-	const lastNameHandler = event => {
-		setLastName(event.target.value)
-	};
+	useLogger(input.value)
 
 	return (
 		<div className={'container pt-3'}>
-			<input type='text' value={name} onChange={changeHandler} />
-			<input type='text' value={lastName} onChange={lastNameHandler} />
+			{/* <input type='text' value={input.value} onChange={input.onChange} /> */}
+			<input type='text' {...input.bind} />
+			<input type='text' {...lastName.bind} />
+			
+			<button className='btn btn-warning' onClick={() => { input.clear(); lastName.clear(); }}>Clear fields</button>
 			<hr />
-			<h1>{name} {lastName}</h1>
+			<h1>{input.value} {lastName.value}</h1>
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
