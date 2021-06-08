060b70bdb3858f0760386cf02d1eb4a9a0c526c5
Valeria Kan <kan.valeria1309@gmail.com>
2021-06-08 08:58:16 -0600

    hook 'useContext' - refactored: pass object as value in AlertContext

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {useAlertToggle} from './alert/AlertContext';
+import {useAlert} from './alert/AlertContext';
 
 export default function Main() {
-	const toggle = useAlertToggle();
+	const {toggle} = useAlert();
 	return (
 		<div>
 			<h1>Sample with Context</h1>
