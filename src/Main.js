312925c9f91f18c8f23c43d421e9ee787e5979ed
Valeria Kan <kan.valeria1309@gmail.com>
2021-06-08 12:43:34 -0600

    hook 'useReducer'

--- src/Main.js
+++ src/Main.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {useAlert} from './alert/AlertContext';
 
 export default function Main() {
-	const {toggle} = useAlert();
+	const {show} = useAlert();
 	return (
 		<div>
 			<h1>Sample with Context</h1>
-			<button onClick={toggle} className='btn btn-success'>Show alert</button>
+			<button onClick={() => show('This text is from Main.js')} className='btn btn-success'>Show alert</button>
 		</div>
 	)
 }
